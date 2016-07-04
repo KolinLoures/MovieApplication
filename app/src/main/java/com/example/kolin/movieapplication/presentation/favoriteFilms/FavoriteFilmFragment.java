c93a3b56d2d8af62b60320289c60bdfa90ab19b7
n.kirilov <kolin-1994@mail/ru>
2016-07-04 15:01:40 +0300

    fixed some problems

--- app/src/main/java/com/example/kolin/movieapplication/presentation/favoriteFilms/FavoriteFilmFragment.java
+++ app/src/main/java/com/example/kolin/movieapplication/presentation/favoriteFilms/FavoriteFilmFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -86,6 +87,7 @@ public class FavoriteFilmFragment extends BaseFragment implements Contract.ViewF
             public void onClickDelete(View v, int position) {
                 switch (v.getId()){
                     case R.id.favorite_button:
+                        Snackbar.make(getView(), "Deleted from favorite!", Snackbar.LENGTH_LONG).show();
                         presenter.removeFavoriteFilm(context, adapter.getItem(position));
                         break;
                     default:
